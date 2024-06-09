9e4499476d251c8d819cb924e503c1051a70d09c
Lauri Jouaux <lauri.jouaux@outlook.com>
2024-06-09 17:31:28 +0200

    Redis result cache works

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,13 @@
-import time
 import asyncio
 import requests
-import aioredis
+import redis.asyncio as redis
 import pika
 import datetime
 
-async def cache_games(year):
+def cache_games(year):
     cache = redis.Redis(host='redis', port=6379)
     game_data_api_url = "https://api.openligadb.de/getmatchdata/bl1/"
-    resp = requests.get( game_data_api_url+str(year) , params=params)
+    resp = requests.get( game_data_api_url+str(year))
     games = resp.json()
     for game in games:
         game.get("")
@@ -21,58 +20,98 @@ async def get_games():
                 if retries == 0:
                     raise exc
     
-def calculate_frequency():
+async def init_results(cache):
     """Reads"""
-    year = Datetime.now().year
-    cache = redis.Redis(host='redis', port=6379)
+    print("Backend Running")
+    year = datetime.datetime.now().year
     game_data_api_url = "https://api.openligadb.de/getmatchdata/bl1/"
-    x =year
+    x = year
     while True:
-
-        resp = requests.get( game_data_api_url+str(year))
+        print(x)
+        resp = requests.get( game_data_api_url+str(x))
         games = resp.json()
         if len(games)<= 0 and year != x:
             break
         for game in games:
             team1_name = game.get("team1",{}).get("teamName")
-            team2_name = game.get("team2",{}).get("teamName") 
-            results = game.get("matchResults",None)
-            if results is not None:
+            team2_name = game.get("team2",{}).get("teamName")
+            print(team1_name)
+            results = game.get("matchResults",[])
+            #print(results)
+            if len(results)>0:          
                 endresult = results[-1]
                 team1_points = endresult.get("pointsTeam1")
                 team2_points = endresult.get("pointsTeam2")
-                cache.
+                team_names = sorted([team1_name,team2_name])
+                key = team_names[0]+":"+team_names[1]
+                teams = {team1_name:team1_points,team2_name:team2_points}
+                teams = dict(sorted(teams.items()))
+                result_data = await cache.hgetall(key)
+                print(key)
+                print(result_data)
+                
+                if not isinstance(result_data,dict):
+                    result_data = {
+                    b'team_a_wins': 0,
+                    b'team_b_wins': 0,
+                    b'tie': 0,
+                    }
+                else:
+                    print(result_data)
+                if list(teams.values())[0]> list(teams.values())[1]:
+                    result_data.update({b'team_a_wins': int(result_data.get(b'team_a_wins',0)) +1 } )
+                else: 
+                    if list(teams.values())[0] == list(teams.values())[1]:
+                        result_data.update({b'tie': int(result_data.get(b'tie',0)) +1 } )
+                    else: 
+                        result_data.update({b'team_b_wins': int(result_data.get(b'team_b_wins',0)) +1 } )
+                await cache.hset(key, mapping=result_data)
             else:
                 break   
         x=x-1
-def get_frequency(ch, method, properties, body):
-        while True:
-            try:
-                return cache.get(body)
-            except redis.exceptions.ConnectionError as exc:
-                if retries == 0:
-                    raise exc
+    print("init finished")
+    
+async def give_frequency(cache,team_a,team_b,bet):
+    """ """
+        team_names = sorted([team_a,team_b])
+        key = team_names[0]+":"+team_names[1]
+        try:
+             results = await cache.get(key)
+        except redis.exceptions.ConnectionError as exc:
+        if team_names[0] == team_a:
+            devisor =             
+
    
 
-def consume_queues():
+async def consume_queues(channel):
     channel.basic_consume(queue='frequency',
                       auto_ack=True,
                       on_message_callback=get_frequency)
     channel.basic_consume(queue='games',
                       auto_ack=True,
-                      on_message_callback=get_game_day)
+                      on_message_callback=get_game)
     
 def main():
-    redis_pool = await aioredis.create_redis_pool('redis://redis', encoding='utf8')
-    cache_games(Datetime.now().year)
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-    channel = connection.channel()
-    calculate_frequency(cache)
-    consume_queues(channel)
-    while True:
-        events = await pool.xread(['wins_stream'], latest_ids=[last_id], timeout=0, count=10)
-        # Process each event by calling `add_new_win`
-        for _, e_id, e in events:
-            winner = e['winner']
-            await add_new_win(pool, winner)
-            last_id = e_id
+    redis_pool = redis.ConnectionPool.from_url("redis://redis")
+    cache_games(datetime.datetime.now().year)
+    #connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672,
+    #                                   '/',))
+    #channel = connection.channel()
+    connection_1 = redis.Redis(connection_pool=redis_pool)
+    asyncio.run(calculate_frequency(connection_1))
+
+    #consume_queues(channel)
+    #asyncio.run(init_redis_pool())
+
+async def init_redis_pool():
+        while True:
+            events = await pool.xread(['wins_stream'], latest_ids=[last_id], timeout=0, count=10)
+            # Process each event by calling `add_new_win`
+            for _, e_id, e in events:
+                winner = e['winner']
+                await add_new_win(pool, winner)
+                last_id = e_id
+            
+if __name__=="__main__": 
+    main() 
+
